b3596017c83cf959f1bba4d9ec6111dacca0ece6
Shades <vicshade12@gmail.com>
2026-09-15 13:10:08 +0100

    fixed endpoints

--- src/server.js
+++ src/server.js
@@ -17,7 +17,7 @@
     const PORT = process.env.PORT || 5050;
 
     //middleware
-    app.use(cors())
+    app.use(cors({ origin: true, credentials: true }))
     app.use(express.json())
     app.use(express.urlencoded({ extended: true }))
     app.use(express.static(__dirname))
--- src/utils/sendMail.js
+++ src/utils/sendMail.js
@@ -1,12 +1,19 @@
 const transporter = require('../../config/mail');
 
 const sendMail = async ({to, subject, html}) => {
-  await transporter.sendMail({
-    from: `"${process.env.EMAIL_FROM_NAME}" <${process.env.EMAIL_USER}>`,
-    to,
-    subject,
-    html
-  });
+  try {
+    const info = await transporter.sendMail({
+      from: `"${process.env.EMAIL_FROM_NAME}" <${process.env.EMAIL_USER}>`,
+      to,
+      subject,
+      html
+    });
+    console.log("Email sent successfully:", info.response);
+    return info;
+  } catch (err) {
+    console.error("Nodemailer Error:", err);
+    throw err;
+  }
 };  
 
 module.exports = sendMail;
